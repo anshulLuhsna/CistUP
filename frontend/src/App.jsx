cf8db2c3826f75c84533ca284e0a84657f5dcfe3
anshulLuhsna <anshulkalbande@gmail.com>
2024-03-24 21:17:58 +0530

    changed some text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -130,8 +130,8 @@ function App() {
             <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
             </svg>
-            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400 comfortaa-regular"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-            <p class="text-xs text-gray-500 dark:text-gray-400 comfortaa-regular">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400 comfortaa-regular"><span class="font-semibold">Click to upload</span> or drag and drop on the button</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400 comfortaa-regular"></p>
         </div>
         <input id="dropzone-file" type='file' className='ml-12 comfortaa-regular' onChange={handleImageUpload} ></input>
     </label>
